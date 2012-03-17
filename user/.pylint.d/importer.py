270588b039b3ad39cfe2860200dd6856f8b05686
Enrico Tröger <enrico.troeger@uvena.de>
2012-03-17 18:26:22 +0100

    Consider eggs at the top level of projects when building the import path

--- user/.pylint.d/importer.py
+++ user/.pylint.d/importer.py
@@ -6,6 +6,18 @@ import sys
 import os
 
 
+#----------------------------------------------------------------------
+def add_eggs(path):
+    egg_base_path = os.path.join(path, 'eggs')
+    if not os.path.exists(egg_base_path):
+        return
+    for egg in os.listdir(egg_base_path):
+        egg_path = os.path.join(egg_base_path, egg)
+        if os.path.exists(egg_path):
+            sys.path.insert(1, egg_path)
+
+
+#----------------------------------------------------------------------
 def fix_paths():
     cwd = os.getcwd()
     cwd_parts = cwd.split('/')
@@ -15,22 +27,25 @@ def fix_paths():
         include_path = '/'.join(cwd_parts[:-1]) + '/include'
         if os.path.exists(include_path):
             sys.path.insert(0, include_path)
+            add_eggs('/'.join(cwd_parts[:-1]))
     elif os.path.exists(os.path.join(cwd, 'include')):
         include_path = os.path.join(cwd, 'include')
         if os.path.exists(include_path):
             sys.path.insert(0, include_path)
+            add_eggs(cwd)
     else:
         while idx >= 0 and cwd_parts[idx] != 'include' and cwd_parts[idx] != '/':
             idx -= 1
             # if we find a license file, assume this is the top-level directory
-            if os.path.exists(os.path.join('/'.join(cwd_parts[:idx+1]), 'COPYING')):
+            if os.path.exists(os.path.join('/'.join(cwd_parts[:idx + 1]), 'COPYING')):
                 break
 
-        include_path_parts = cwd_parts[:idx+1]
+        include_path_parts = cwd_parts[:idx + 1]
         if include_path_parts:
             include_path = '/'.join(include_path_parts)
             if os.path.exists(include_path):
                 sys.path.insert(0, include_path)
+                add_eggs('/'.join(include_path_parts[:-1]))
 
 
 fix_paths()
