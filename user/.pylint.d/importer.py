d5e2e9593b7e765cf82ded5e9bc2b55d3250b043
Enrico Tröger <enrico.troeger@uvena.de>
2012-04-03 00:09:11 +0200

    Try to detect virtual envs in Django projects
    This works only with Django 1.4 like projects with the manage.py lying
    in the top root directory next to the virtualenv named 'env' or 'venv'.
    
    P.S.: I really should rewrite this code to be less ugly and actually readable.

--- user/.pylint.d/importer.py
+++ user/.pylint.d/importer.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
+import glob
 import os
+import sys
 
 
 #----------------------------------------------------------------------
@@ -18,17 +19,35 @@ def add_eggs(path):
 
 
 #----------------------------------------------------------------------
+def detect_and_add_virtualenv_for_django(path):
+    # look for virtualenv
+    env_path = os.path.join(path, 'env')
+    if not os.path.exists(env_path):
+        env_path = os.path.join(path, 'venv')
+        if not os.path.exists(env_path):
+            # gire up
+            return
+    # use glob to find the path with either of Python 2.6 or 2.7
+    site_packages_path = u'%s/lib/python2.?/site-packages' % env_path
+    site_packages = glob.glob(site_packages_path)
+    if len(site_packages) == 1:
+        sys.path.extend(site_packages)
+
+
+#----------------------------------------------------------------------
 def fix_paths():
     cwd = os.getcwd()
     cwd_parts = cwd.split('/')
     idx = len(cwd_parts) - 1
 
     if cwd_parts[-1] == 'bin':
+        # in the bin/ directory
         include_path = '/'.join(cwd_parts[:-1]) + '/include'
         if os.path.exists(include_path):
             sys.path.insert(0, include_path)
             add_eggs('/'.join(cwd_parts[:-1]))
     elif os.path.exists(os.path.join(cwd, 'include')):
+        # on top-level project directory
         include_path = os.path.join(cwd, 'include')
         if os.path.exists(include_path):
             sys.path.insert(0, include_path)
@@ -39,6 +58,9 @@ def fix_paths():
             # if we find a license file, assume this is the top-level directory
             if os.path.exists(os.path.join('/'.join(cwd_parts[:idx + 1]), 'COPYING')):
                 break
+            # if we find a manage.py, this is most certaily the top of a Django (1.4) project
+            if os.path.exists(os.path.join('/'.join(cwd_parts[:idx + 1]), 'manage.py')):
+                break
 
         include_path_parts = cwd_parts[:idx + 1]
         if include_path_parts:
@@ -46,6 +68,7 @@ def fix_paths():
             if os.path.exists(include_path):
                 sys.path.insert(0, include_path)
                 add_eggs('/'.join(include_path_parts[:-1]))
+            detect_and_add_virtualenv_for_django(include_path)
 
 
 fix_paths()
