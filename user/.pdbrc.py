4d3293f14fe40efb813a400b37805fe9e9c29220
Enrico Tröger <enrico.troeger@uvena.de>
2019-11-24 23:01:17 +0100

    PDB Python3 migration

--- user/.pdbrc.py
+++ user/.pdbrc.py
@@ -1,4 +1,5 @@
 # Command line history:
+import os
 import readline
 
 HISTFILE = os.path.expanduser("~/.pdbhist")
@@ -10,8 +11,14 @@ except IOError:
 import atexit
 atexit.register(readline.write_history_file, HISTFILE)
 readline.set_history_length(1000)
-
-# cleanup
 del HISTFILE
-del readline
-del atexit
+
+# Cleanup any variables that could otherwise clutter up the namespace.
+try:
+    del atexit
+    del os
+    del readline
+    del rlcompleter
+except NameError:
+    # Probably this is a second pdbrc that has been loaded.
+    pass
