c69dd8d6191f0bbf6622895f4eaf1f4e7aaf3999
Enrico Tröger <enrico.troeger@uvena.de>
2020-05-02 17:12:33 +0200

    Pimp the export script: show diff and ask before overwriting files

--- export.sh
+++ export.sh
@@ -1,11 +1,47 @@
 #!/usr/bin/zsh
 
-cd $(dirname $0)
+CWD=$(dirname $0)
+cd $CWD
 
 
+function confirm() {
+	read -r -q "response?${1:-Are you sure? [y/N]}?"
+	echo
+	if [ "${response}" = "y" ];
+	then
+		true
+	else
+		false
+	fi
+}
+
+
+# we need you, ZSH
+if [ -z "$ZSH_VERSION" ]; then
+	echo "This script needs to be executed in ZSH!"
+	exit 1
+fi
+
+# start the party
 git pull
-
-for file in $(ls -A user/)
+for file in $(find user -type f -printf '%P ')
 do
-	cp -r "user/${file}" ~/
+	old_file="${HOME}/${file}"
+	new_file="${CWD}/user/${file}"
+
+	# get the diff
+	file_diff="$(diff -Naur "${old_file}" "${new_file}")"
+	if [ "${file_diff}" ]; then
+		echo "File ${new_file} has changed:\n${file_diff}"
+		# copy file only if confirmed by user
+		if confirm "Do you want to replace it? [y/N]"; then
+			old_file_dirname="$(dirname "${old_file}")"
+			# create target directory if it doesn't exist yet
+			if [ ! -d "${old_file_dirname}" ]; then
+				mkdir -p "${old_file_dirname}"
+			fi
+			cp "${new_file}" "${old_file}"
+			echo "${new_file} replaced with new version"
+		fi
+	fi
 done
